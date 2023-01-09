b53efc066a0f53b1620455f24f3dc2aaa7487ef0
George Manaz <george@MacBook-Air-som-tillhor-George.local>
2023-01-09 23:04:53 +0100

    added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled, { css } from 'styled-components/macro';
+import { Link } from 'react-router-dom';
+import { MenuData } from '../data/MenuData.js';
+
+const Nav = styled.nav`
+    height: 60px;
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 2rem;
+    z-index: 100;
+    position: fixed;
+    width: 100%;
+    background: #000;
+`;
+
+const NavLink = css`
+    color: #fff;
+    display: flex;
+    align-items: center;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+    text-decoration: none;
+`;
+
+const MenuBars = styled.i``;
+
+const NavMenu = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const NavMenuLinks = styled(Link)`
+    ${NavLink}
+`;
+
+const Navbar = () => {
+  return (
+    <Nav>
+        <MenuBars />
+        <NavMenu>
+            { MenuData.map((item, index) => (
+                <NavMenuLinks to={item.link} key={index}>
+                    {item.title}
+                </NavMenuLinks>
+            )) }
+        </NavMenu>
+    </Nav>
+  )
+}
+
+export default Navbar
